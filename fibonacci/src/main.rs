fb509d670bb281a4eb2bf0eb8de73167e05cda25
James McKee <james@mck.is>
2022-05-15 21:48:33 +0100

    Change numbers slightly to make it look nicer

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let mut numbers: Vec<u128> = vec![0, 1];
 
-    // 186 is as high as you can go with u128 before overflows
-    for count in 1..186 {
-        numbers.push(numbers[count] + numbers[count - 1]);
+    // 185 is as high as you can go with u128 before overflows
+    for count in 0..185 {
+        numbers.push(numbers[count] + numbers[count + 1]);
     }
     println!("{:#?}", numbers)
 }
